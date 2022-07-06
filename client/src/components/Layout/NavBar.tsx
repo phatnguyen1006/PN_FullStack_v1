4fbcb34ccf7ad9255711da9ca4e397966cbba646
P <phatnguyen876@gmail.com>
2022-07-06 14:25:06 +0700

    Update cache when logout

--- client/src/components/Layout/NavBar.tsx
+++ client/src/components/Layout/NavBar.tsx
@@ -1,73 +1,104 @@
+import { gql, Reference } from "@apollo/client";
 import { Box, Button, Flex, Heading, Link } from "@chakra-ui/react";
 import NextLink from "next/link";
 import {
-  MeDocument,
-  MeQuery,
-  useLogoutMutation,
-  useMeQuery,
+    MeDocument,
+    MeQuery,
+    useLogoutMutation,
+    useMeQuery,
 } from "../../generated/graphql";
 
 const NavBar = () => {
-  const { data, loading: useMeQueryLoading } = useMeQuery();
-  const [logout, { loading: useLogoutMutationLoading }] = useLogoutMutation();
+    const { data, loading: useMeQueryLoading } = useMeQuery();
+    const [logout, { loading: useLogoutMutationLoading }] = useLogoutMutation();
 
-  const logoutUser = async () => {
-    await logout({
-      update(cache, { data }) {
-        if (data?.logout) {
-          cache.writeQuery<MeQuery>({
-            query: MeDocument,
-            data: { me: null }, // reset cache
-          });
+    const logoutUser = async () => {
+        await logout({
+            update(cache, { data }) {
+                if (data?.logout) {
+                    cache.writeQuery<MeQuery>({
+                        query: MeDocument,
+                        data: { me: null }, // reset cache
+                    });
+
+                    cache.modify({
+                        fields: {
+                            posts(existing) {
+                                existing.paginatedPosts.forEach((post: Reference) => {
+                                        cache.writeFragment({
+                                            id: post.__ref, // `Post:17`
+                                            fragment: gql`
+                                                fragment VoteType on Post {
+                                                    voteType
+                                                }
+                                            `,
+                                            data: {
+                                                voteType: 0,
+                                            },
+                                        });
+                                    });
+
+                                return existing;
+                            },
+                        },
+                    });
+                }
+            },
+        });
+    };
+
+    const TrailingBarButton = (): JSX.Element => {
+        let body = <></>;
+
+        if (useMeQueryLoading) {
+            body = <></>;
+        } else if (!data?.me) {
+            body = (
+                <>
+                    <NextLink href="/login">
+                        <Link mr={2}>Login</Link>
+                    </NextLink>
+                    <NextLink href="/register">
+                        <Link>Register</Link>
+                    </NextLink>
+                </>
+            );
+        } else {
+            body = (
+                <Flex>
+                    <NextLink href="/create-post">
+                        <Button mr={4}>Create Post</Button>
+                    </NextLink>
+                    <Button
+                        onClick={logoutUser}
+                        isLoading={useLogoutMutationLoading}
+                    >
+                        Logout
+                    </Button>
+                </Flex>
+            );
         }
-      },
-    });
-  };
 
-  const TrailingBarButton = (): JSX.Element => {
-    let body = <></>;
+        return body;
+    };
 
-    if (useMeQueryLoading) {
-      body = <></>;
-    } else if (!data?.me) {
-      body = (
-        <>
-          <NextLink href="/login">
-            <Link mr={2}>Login</Link>
-          </NextLink>
-          <NextLink href="/register">
-            <Link>Register</Link>
-          </NextLink>
-        </>
-      );
-    } else {
-      body = (
-        <Flex>
-          <NextLink href="/create-post">
-            <Button mr={4}>Create Post</Button>
-          </NextLink>
-          <Button onClick={logoutUser} isLoading={useLogoutMutationLoading}>
-            Logout
-          </Button>
-        </Flex>
-      );
-    }
-
-    return body;
-  };
-
-  return (
-    <Box bg="tan" p={4}>
-      <Flex maxW={800} m="auto" justifyContent="space-between" align="center">
-        <NextLink href="/">
-          <Heading>Reddit</Heading>
-        </NextLink>
-        <Box>
-          <TrailingBarButton />
+    return (
+        <Box bg="tan" p={4}>
+            <Flex
+                maxW={800}
+                m="auto"
+                justifyContent="space-between"
+                align="center"
+            >
+                <NextLink href="/">
+                    <Heading>Reddit</Heading>
+                </NextLink>
+                <Box>
+                    <TrailingBarButton />
+                </Box>
+            </Flex>
         </Box>
-      </Flex>
-    </Box>
-  );
+    );
 };
 
 export default NavBar;
